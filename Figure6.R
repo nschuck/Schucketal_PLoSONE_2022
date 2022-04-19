1ee640d75205d261654c3d48ed0eea69260c298d
Nico Schuck <nico.schuck@gmail.com>
2022-04-19 16:44:06 +0200

    removed figure files; added necessary code to Figure6.R

--- Figure6.R
+++ Figure6.R
@@ -24,6 +24,10 @@ axisline = 2
 ctab = aggregate(scores.cdf[,c(20:25, 28:29, 32:33)], list(scores.cdf$GROUPbin), mean, na.rm = TRUE)
 ctab_ses = aggregate(scores.cdf[,c(20:25, 28:29, 32:33)], list(scores.cdf$GROUPbin), std.error)
 
+scores.cdf.no.na = na.omit(scores.cdf)
+
+fullmod = lm(FOLL_logit ~ GROUPbin*(STROOPz+TASKz+PREz+WMz), data=scores.cdf.no.na) #AMBIG can also be included
+
 
 pdf('plots/Fig6A_Overview.pdf', width = 4.25, height = 4)
 
@@ -62,8 +66,6 @@ X = predict(fullmod)
 Y = scores.cdf.no.na$FOLL_logit*1
 lm_kids = lm(Y[scores.cdf.no.na$GROUPbin == 'KIDS']~X[scores.cdf.no.na$GROUPbin == 'KIDS'])
 lm_ya = lm(Y[scores.cdf.no.na$GROUPbin == 'YA']~X[scores.cdf.no.na$GROUPbin == 'YA'])
-summary(lm_kids)
-summary(lm_ya)
 
 ccols = c(colorset[,2])[as.numeric(scores.cdf.no.na$GROUPbin)]
 plot(X, Y,type = 'p', col = ccols,
@@ -76,7 +78,6 @@ axis(1, at = seq(-1, 5, length.out = 5), cex.axis = 1.3)
 mtext(1, text = 'Predicted Color Use ', line = 2.7, cex = 1.2)
 axis(2, at = seq(-1, 5, length.out = 5), cex.axis = 1.3)
 mtext(2, text = 'Color Use (logit)', line = 2.7, cex = 1.2)
-
 
 plot(scores.cdf.no.na$TASKz[scores.cdf.no.na$GROUPbin == 'YA'], scores.cdf.no.na$FOLL_logit[scores.cdf.no.na$GROUPbin == 'YA']*1,
 	type = 'p', col = colorset[2,2],
